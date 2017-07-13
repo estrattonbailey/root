411fae56fbadade710b3d47bbd80bcdc78b1f459
estrattonbailey <ericstrattonbailey@gmail.com>
2017-07-12 21:21:30 -0400

    Update hydrate dep, remove package name from project header

--- app/components/ProjectHeader.js
+++ app/components/ProjectHeader.js
@@ -1,22 +1,22 @@
 import React from 'react'
 import Link from 'react-hydrate-link'
-import { Container } from 'Components/Layout'
+import { Outer, Container } from 'Components/Layout'
 
 const ProjectHeader = ({ path }) => {
   return (
     <div className='abs top left right'>
-      <div className='ph2 pt2'>
-        <Container>
-          <header className='header--project mt2'>
-            <div className='f fw'>
-              <Link to='/' className='__text'>
-                <h2 className='h4 mv0'>estrattonbailey</h2>
-              </Link>
-              <span className='h4 mh05 __text'>/</span>
-              <h3 className='h4 mv0 __text'>{path}</h3>
-            </div>
-          </header>
-        </Container>
+      <div className='pt2'>
+        <Outer>
+          <Container>
+            <header className='header--project mt2'>
+              <div className='f fw'>
+                <Link to='/' className='__text'>
+                  <h2 className='h4 mv0'>estrattonbailey</h2>
+                </Link>
+              </div>
+            </header>
+          </Container>
+        </Outer>
       </div>
     </div>
   )
